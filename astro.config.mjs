651df95b93e9ac509104c70e42acb4d18a38d181
Albert Escamilla Garcia <albert@MacBook-Pro-de-Albert.local>
2024-02-23 11:53:20 +0100

    add new comment on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ export default defineConfig({
   integrations: [tailwind()],
   
   //👇 Configuramos el renderizado estatico o no de la aplicaicón. Las opciones son "static", "server" o "hybrid"
+  //   Cuando utilizamos "server" o "hybrid" vamos a tener que instalar un adaptador para el servidor que vamos a utilizar
+  //   Más información en https://docs.astro.build/es/guides/server-side-rendering/
   output: "static", 
 });
