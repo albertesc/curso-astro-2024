755493961e84da9ddcabc2c11cf0e1c8ba66eb52
Albert Escamilla Garcia <albert@MacBook-Pro-de-Albert.local>
2024-02-23 11:55:22 +0100

    add new comment on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ export default defineConfig({
   integrations: [tailwind(), react(), vue()],
 
   //👇 Configuramos el renderizado estatico o no de la aplicaicón. Las opciones son "static", "server" o "hybrid"
+  //   Cuando utilizamos "server" o "hybrid" vamos a tener que instalar un adaptador para el servidor que vamos a utilizar
+  //   Más información en https://docs.astro.build/es/guides/server-side-rendering/
   output: "static",
 
   //👇 Configuramos el i18n
@@ -19,9 +21,14 @@ export default defineConfig({
     // 👈 marcamos los idiomas que vamos a usar
 
     // 👇 Configuramos la manera de comportarse de las rutas para los idiomas. 
-    // Por ejemplo en este proyecto para ingles la ruta será "/" y para español la ruta será "/es"
+    //    Por ejemplo en este proyecto para ingles la ruta será "/" y para español la ruta será "/es"
     routing: {
       prefixDefaultLocale: false // 👈 en caso de poner true, la ruta por defecto será "/en"
+    },
+
+    // 👇 Configuramos el fallback de los idiomas
+    fallback: {
+      'es': 'en' // 👈 en caso de que no se encuentre una ruta en español, se redirigirá a la ruta en inglés
     }
   }
 });
